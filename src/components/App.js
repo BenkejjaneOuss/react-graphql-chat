99e7c14cd534fe32feca9e4416b08fec98859fb1
Oussama Benkejjane <oussama.benkejjane@gmail.com>
2019-09-28 13:22:16 +0100

    updating component to create new user

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react'
+import { withApollo, graphql } from 'react-apollo'
+import * as compose from 'lodash.flowright'
+import gql from 'graphql-tag'
+import generateUsername from 'sillyname'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const createUser = gql`
+    mutation createUser($name: String!) {
+        createUser(name: $name) {
+            id
+            name
+        }
+    }
+`
+
+const REACT_GRAPHQL_CHAT_ID_KEY = 'REACT_GRAPHQL_CHAT_ID'
+
+class App extends Component {
+
+  state = {
+    userId: undefined,
+  }
+
+  async componentDidMount() {
+    
+    
+    const userId = localStorage.getItem(REACT_GRAPHQL_CHAT_ID_KEY)
+
+    // Check if user already exists
+    if (!Boolean(userId)) {
+      this._createNewUser()
+    }
+    else {
+      this._updateExistingUser(userId)
+    }
+  }
+
+  _createNewUser = () => {
+    this.props.createUserMutation({
+      variables: {
+        name: generateUsername(),
+      }
+    }).then(result => {
+      localStorage.setItem(REACT_GRAPHQL_CHAT_ID_KEY, result.data.createUser.id)
+      this.setState({
+        userId: result.data.createUser.id,
+      })
+    })
+  }
+
+  _updateExistingUser = async (userId) => {
+
+    this.setState({
+      userId: userId
+    })
+
+  }
+
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default compose(
+  graphql(createUser, {name: 'createUserMutation'})
+)(withApollo(App))
