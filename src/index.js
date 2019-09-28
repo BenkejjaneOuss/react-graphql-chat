9fff8c20a218484926b8982669b1f99b3dd3df49
Oussama Benkejjane <oussama.benkejjane@gmail.com>
2019-09-28 13:12:01 +0100

    connecting the app with GraphQL API

--- src/index.js
+++ src/index.js
@@ -1,10 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { split } from 'apollo-link'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
+import App from './components/App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Paste your own service ID here
+const serviceId = `ck129nhui87ak0194z1hqulri`
+
+const httpLink = createHttpLink({ uri: `https://api.graph.cool/simple/v1/${serviceId}` })
+
+const wsLink = new WebSocketLink({
+  uri: `wss://subscriptions.graph.cool/v1/${serviceId}`,
+  options: {
+    reconnect: true
+  }
+})
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  httpLink,
+)
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache().restore(window.__APOLLO_STATE__),
+})
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App/>
+    </ApolloProvider>, 
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
